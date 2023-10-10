8d0d83f736025f4345e6bbded3f77db463953c1d
nunera <67439647+nunera@users.noreply.github.com>
2023-10-10 12:06:44 -0500

    wsg

--- Neo.lua
+++ Neo.lua
@@ -1,9 +1,69 @@
-local UI = loadstring("https://raw.githubusercontent.com/nunera/Neo/main/UI.lua")
+local V = "0.0.1"
 
-local LegitTab = UI.library:AddTab("Legit"); 
-local LegitColunm1 = LegitTab:AddColumn();
-local LegitMain = LegitColunm1:AddSection("Aim Assist")
+local UI = loadstring(game:HttpGet("https://raw.githubusercontent.com/nunera/Neo/main/UI/Library.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/nunera/Neo/main/UI/SaveManager.lua"))()
+local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/nunera/Neo/main/UI/ThemeManager.lua"))()
+
+local Window = UI:CreateWindow({
+    Title = "Neo - V"..V,
+    Center = true,
+    AutoShow = true,
+    TabPadding = 8,
+    MenuFadeTime = 0.2
+})
+
+local Tabs = {
+    Main = Window:AddTab('Main'),
+    ['Settings'] = Window:AddTab('Settings')
+}
+
+Library:SetWatermarkVisibility(true)
+
+local FrameTimer = tick()
+local FrameCounter = 0;
+local FPS = 144;
+
+local WatermarkConnection = game:GetService('RunService').RenderStepped:Connect(function()
+    FrameCounter += 1;
+
+    if (tick() - FrameTimer) >= 1 then
+        FPS = FrameCounter;
+        FrameTimer = tick();
+        FrameCounter = 0;
+    end;
+
+    Library:SetWatermark(('Neo V%v| %s fps | %s ms'):format(V,
+        math.floor(FPS),
+        math.floor(game:GetService('Stats').Network.ServerStatsItem['Data Ping']:GetValue())
+    ));
+end);
+
+Library.KeybindFrame.Visible = true;
+
+Library:OnUnload(function()
+    WatermarkConnection:Disconnect()
+
+    print('Unloaded!')
+    Library.Unloaded = true
+end)
+local MenuGroup = Tabs['Settings']:AddLeftGroupbox('Menu')
 
 
-UI.library:Init();
-UI.library:selectTab(library.tabs[1]);
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+
+Library.ToggleKeybind = Options.MenuKeybind 
+
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+
+ThemeManager:SetFolder('Neo')
+SaveManager:SetFolder('Neo/Saves/'..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
+
+SaveManager:BuildConfigSection(Tabs['Settings'])
+ThemeManager:ApplyToTab(Tabs['Settings'])
+
+SaveManager:LoadAutoloadConfig()
