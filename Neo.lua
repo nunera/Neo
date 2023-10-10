d3e7e3d56a662ba2848f3f2a3cb080612933f03f
nunera <67439647+nunera@users.noreply.github.com>
2023-10-10 16:34:05 -0500

    a

--- Neo.lua
+++ Neo.lua
@@ -32,37 +32,42 @@ local Tabs = {
 Library:SetWatermarkVisibility(true)
 
 local FrameTimer = tick()
-local FrameCounter = 0;
-local FPS = 144;
+local FrameCounter = 0
+local FPS = 144
 
 local WatermarkConnection = game:GetService('RunService').RenderStepped:Connect(function()
-    FrameCounter += 1;
+    FrameCounter += 1
 
     if (tick() - FrameTimer) >= 1 then
-        FPS = FrameCounter;
-        FrameTimer = tick();
-        FrameCounter = 0;
-    end;
+        FPS = FrameCounter
+        FrameTimer = tick()
+        FrameCounter = 0
+    end
 
-    Library:SetWatermark(('Neo V%s| %s fps | %s ms'):format(V,
+    Library:SetWatermark(('Neo V%s| %s fps | %s ms'):format(
+        V,
         math.floor(FPS),
         math.floor(game:GetService('Stats').Network.ServerStatsItem['Data Ping']:GetValue())
-    ));
-end);
-
-Library.KeybindFrame.Visible = true;
+    ))
+end)
 
 Library:OnUnload(function()
     WatermarkConnection:Disconnect()
-
-    print('Unloaded!')
+    getgenv().Neo = nil
     Library.Unloaded = true
 end)
 local MenuGroup = Tabs['Settings']:AddLeftGroupbox('Menu')
 
-
 MenuGroup:AddButton('Unload', function() Library:Unload() end)
-MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'Insert', NoUI = true, Text = 'Menu keybind' })
+MenuGroup:AddToggle('Keybinds', {
+    Text = 'Keybinds',
+    Default = false,
+
+    Callback = function(value)
+        Library.KeybindFrame.Visible = value
+    end
+})
 
 Library.ToggleKeybind = Options.MenuKeybind 
 
@@ -73,9 +78,11 @@ SaveManager:IgnoreThemeSettings()
 SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
 
 ThemeManager:SetFolder('Neo')
-SaveManager:SetFolder('Neo/Saves/'..GameId)
+SaveManager:SetFolder('Neo/Saves/'..game.GameId)
 
 SaveManager:BuildConfigSection(Tabs['Settings'])
 ThemeManager:ApplyToTab(Tabs['Settings'])
 
 SaveManager:LoadAutoloadConfig()
+getgenv().Neo = true
+
