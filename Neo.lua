1b174aa40b8bf7b4a4c8be47e1a59cfb975ad9dc
nunera <67439647+nunera@users.noreply.github.com>
2023-10-09 22:39:23 -0500

    e

--- Neo.lua
+++ Neo.lua
@@ -1,3 +1,5 @@
 UI = loadstring("https://raw.githubusercontent.com/nunera/Neo/main/UI.lua")
 
 local tab = UI.library:AddTab("Test")
+local LegitColunm1 = tab:AddColumn();
+local LegitMain = LegitColunm1:AddSection("Aim Assist")
