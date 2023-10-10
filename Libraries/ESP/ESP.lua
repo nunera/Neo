f5044d4e00d72cd53bd7e4e53ff2cea33ffd4228
nunera <67439647+nunera@users.noreply.github.com>
2023-10-10 16:34:17 -0500

    LUYA

--- Libraries/ESP/ESP.lua
+++ Libraries/ESP/ESP.lua
@@ -4,7 +4,7 @@ local select, pcall, loadstring , warn = select, pcall, loadstring, warn
 local Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/nunera/Neo/main/Libraries/ESP/Modules/Original.lua"))))
 
 if not Success then
-    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/nunera/Neo/main/Libraries/ESP/Modules/UWP"))))
+    Success, ESP = pcall(select(2, pcall(loadstring, game:HttpGet("https://raw.githubusercontent.com/nunera/Neo/main/Libraries/ESP/Modules/UWP.lua"))))
 
     if not Success then
         return warn("EXUNYS_ESP > Loader - Your script execution software does not support this module.")
